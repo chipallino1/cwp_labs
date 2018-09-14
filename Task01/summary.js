b7b3f007cdbd9a948063ffe54bd9fd4d3ddd1111
chipallino1 <skorupich00@mail.ru>
2018-09-14 10:23:26 +0300

    finally version

--- Task01/summary.js
+++ Task01/summary.js
@@ -4,33 +4,29 @@ dir='D:/Универ/Учеб/3 курс/пкп/Tasks/Task01';
 getFiles(dir);
 var dirName=currentDirName(dir);
 fs.mkdir(dir+'/'+dirName,()=>{
-	console.log('its ok');
+	console.log('\n---New '+dirName+' created---');
 });
 currentDirName(dir);
 var arr=copyTxtFiles(dir,fs);
-//console.log(arr.length);
-/*for(var i=0;i<arr.length;i++)
-{
-	
-	addCopyright(arr[i],fs);
-}*/
 
 
-var countfiles;
+
+
+
 
 function getFiles(file) {
 fs.stat(file,(err,stats)=>
 {
-console.log(file.replace(new RegExp (dir, 'g'), ''));
-if(stats.isDirectory())
-{
-fs.readdir(file,(err,files)=>{
-for (var i = 0; i <files.length; i++) {
-var dirPath=file+'/'+files[i];
-getFiles(dirPath);
-}
-});
-}
+	console.log('\t'+file.replace(new RegExp (dir, 'g'), ''));
+	if(stats.isDirectory())
+	{
+		fs.readdir(file,(err,files)=>{
+		for (var i = 0; i <files.length; i++) {
+		var dirPath=file+'/'+files[i];
+		getFiles(dirPath);
+			}
+		});
+	}
 })
 }
 
@@ -47,7 +43,7 @@ function checkTxtExtension(file)
 	var extension=file.substring(lastIndex+1);
 	if(extension=='txt')
 	{
-		console.log('txt file');
+		
 		return true;
 	}
 	else
@@ -59,18 +55,17 @@ function copyTxtFiles(dir,fs)
 {
 	var dirName=currentDirName(dir);
 	var destArr=[];
-	//console.log(dirName);
 	fs.readdir(dir,(err,files)=>{
 
 			for(var i=0;i<files.length;i++)
 			{
 				if(checkTxtExtension(files[i]))
 				{
-				var src=dir+'/'+files[i];
-				var dest=dir+'/'+dirName+'/'+files[i];
+				let src=dir+'/'+files[i];
+				let dest=dir+'/'+dirName+'/'+files[i];
 				destArr.push(dest);
 				fs.copyFile(src,dest,(err)=>{
-
+					addCopyright(dest,fs);
 					console.log('coping end  ');
 					
 					});
@@ -79,11 +74,7 @@ function copyTxtFiles(dir,fs)
 				
 				}
 			}
-			addCopyright(destArr[0],fs);
-			addCopyright(destArr[1],fs);
-			addCopyright(destArr[2],fs);
-			addCopyright(destArr[3],fs);
-			//toJSON(destArr,fs);
+			
 
 	});
 	
@@ -99,39 +90,26 @@ function addCopyright(dest,fs)
 	var buffer;
 	
 	fs.readFile(dest, function(error,data){
-            								console.log("Асинхронное чтение файла");
-             								  if(error) throw error; // если возникла ошибка
-             								  			allstring.push(data);
-             										   data='©'+data+'©';
+            								
+        allstring.push(data);
+        data='©'+data+'©';
 
-             										    buffer=Buffer.from(data);
-             										  fs.open(dest, 'r+', function(err, fd) {
+        buffer=Buffer.from(data);
+        fs.open(dest, 'r+', function(err, fd) {
 
-   						 if (err)
-   						  {
-     						   throw 'error opening file: ' + err;
-  							}
-  							//buffer=Buffer.from('hh');
-
-  						  fs.write(fd,buffer,0,buffer.length,0, function(err, written,string) {
-    				   				 if (err) throw 'error writing file: ' + err;
-  						      					fs.close(fd, function() {
-  						      							follow(dest,fs);
-  						      							var out=JSON.stringify(allstring);
-  						      							fs.writeFile("config.json", out, function(err) {
-
-    															if(err) throw err;
-
-  																		  console.log("The file was created!");
-
-																	});
-
-         				  								 console.log('f------------'+out);
+   						
+  		fs.write(fd,buffer,0,buffer.length,0, function(err, written,string) {
+    				   				 
+  			fs.close(fd, function() {
+  				follow(dest,fs);
+  				var out=JSON.stringify(allstring);
+  				fs.writeFile("config.json", out, function(err) { });
+				
        								 });
    						 });
-						});
-              											  //console.log('1111111111111111'+data);  // выводим считанные данные
-														});
+					});
+              											  
+	});
 				
 				
 }
